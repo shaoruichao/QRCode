d96d3765ba2f081d0e5feaa5ad08ae7231960e2e
shaoruichao <shaoruichao@126.com>
2017-02-17 14:50:41 +0800

    描述
    zxing二维码生成和识别

---
+++ app/src/main/java/com/example/qrcode/MainActivity.java
@@ -0,0 +1,102 @@
+package com.example.qrcode;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.qrcode.activity.CaptureActivity;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+
+import java.util.Hashtable;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @InjectView(R.id.et)
+    EditText et;
+    @InjectView(R.id.btn1)
+    Button btn1;
+    @InjectView(R.id.btn2)
+    Button btn2;
+    @InjectView(R.id.iv)
+    ImageView iv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+    }
+
+    @OnClick({R.id.btn1, R.id.btn2})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn1:
+
+                String content = et.getText().toString().trim();
+                try {
+                    if (!TextUtils.isEmpty(content)) {
+                        Bitmap bitmap = Create2DCode(content);
+                        iv.setImageBitmap(bitmap);
+                    } else {
+                        Toast.makeText(MainActivity.this, "请输入要生成的字符串", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (WriterException e) {
+                    e.printStackTrace();
+                }
+
+                break;
+            case R.id.btn2:
+
+                startActivity(new Intent(MainActivity.this, CaptureActivity.class));
+                break;
+        }
+    }
+
+
+    /**
+     * 用字符串生成二维码
+     *
+     * @param str
+     * @author J!nl!n
+     * @return
+     * @throws WriterException
+     */
+    public Bitmap Create2DCode(String str) throws WriterException {
+        Hashtable<EncodeHintType, String> hints = new Hashtable<EncodeHintType, String>();
+        hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+        // 生成二维矩阵,编码时指定大小,不要生成了图片以后再进行缩放,这样会模糊导致识别失败
+        BitMatrix matrix = new MultiFormatWriter().encode(str, BarcodeFormat.QR_CODE, 480, 480, hints);
+        int width = matrix.getWidth();
+        int height = matrix.getHeight();
+        // 二维矩阵转为一维像素数组,也就是一直横着排了
+        int[] pixels = new int[width * height];
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                if (matrix.get(x, y)) {
+                    pixels[y * width + x] = 0xff000000;
+                }
+            }
+        }
+
+        Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        // 通过像素数组生成bitmap,具体参考api
+        bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
+        return bitmap;
+    }
+
+}
